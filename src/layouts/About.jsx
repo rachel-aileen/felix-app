6db739bbd4533188f6e133e7785a07ca42dd3238
rachel-aileen <137586206+rachel-aileen@users.noreply.github.com>
2024-01-12 13:08:20 -0800

    layout 4 practice

--- src/layouts/About.jsx
+++ src/layouts/About.jsx
@@ -1,9 +1,31 @@
+import { Grid } from '@mui/material'
 import React from 'react'
 
 export default function About() {
     return (
         <div className='about'>
 
+
+            <Grid container
+                direction="row-reverse"
+                justifyContent="center"
+                alignItems="center"
+            >
+
+                <Grid item xs={6}
+                >
+                    <img className='row-img' src="https://images.unsplash.com/photo-1519556883520-8a146529e76f?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+
+                </Grid>
+
+                <Grid item xs={6}
+
+                >
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Adipisci fuga, unde placeat officiis saepe enim tempora! Distinctio odit omnis ut.</p>
+                    <button>Submit</button>
+                </Grid>
+
+            </Grid>
 
         </div>
     )
