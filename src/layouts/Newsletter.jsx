cc819acaa9574c212dd5e4128fab679de93b3b01
rachel-aileen <137586206+rachel-aileen@users.noreply.github.com>
2024-01-12 13:03:46 -0800

    layout 4 newsletter practice

--- src/layouts/Newsletter.jsx
+++ src/layouts/Newsletter.jsx
@@ -1,7 +1,26 @@
+import { Grid } from '@mui/material'
 import React from 'react'
 
 export default function Newsletter() {
     return (
-        <div className='newsletter'>Newsletter</div>
+        <div className='newsletter'>
+
+            <Grid container
+                direction="column"
+                justifyContent="center"
+                alignItems="center"
+            >
+
+                <Grid item
+
+                >
+
+                </Grid>
+                <h1>Hello</h1>
+                <h2>Goodbye</h2>
+                <button>Submit</button>
+            </Grid>
+
+        </div>
     )
 }
